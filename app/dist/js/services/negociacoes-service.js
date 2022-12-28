1b1173a098278d1ed029b10746c4b87eddb34f5f
Lucas <lucashenriquefelix@hotmail.com>
2022-12-28 15:39:37 -0300

    Diminuindo a quantidade de console log, e utilizando polimorfismo

--- app/dist/js/services/negociacoes-service.js
+++ app/dist/js/services/negociacoes-service.js
@@ -4,8 +4,8 @@ export class NegociacoesService {
         return fetch('http://localhost:8080/dados')
             .then(res => res.json())
             .then((dados) => {
-            return dados.map(dadosDeHoje => {
-                return new Negociacao(new Date(), dadosDeHoje.vezes, dadosDeHoje.montante);
+            return dados.map(dadoDeHoje => {
+                return new Negociacao(new Date(), dadoDeHoje.vezes, dadoDeHoje.montantes);
             });
         });
     }
